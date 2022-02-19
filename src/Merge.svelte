8e978b3cc3cf87e3ff7c3ce4e16537300cdc0d9f
Lilou Alidor <lilou.alidor@gmail.com>
2022-02-19 14:47:07 +0100

    WIP

--- src/Merge.svelte
+++ src/Merge.svelte
@@ -1,6 +1,7 @@
 <script>
 	import {ComputeMixing} from "./core/recipesUtils"
 	import levelsRecipes from "./data/levels_data"
+	import interact from "interactjs";
 
 	import {flip} from 'svelte/animate';
 	
@@ -41,9 +42,92 @@
 		}
     }
 
+// temp
+
+  function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+// enable draggables to be dropped into this
+interact('.dropzone').dropzone({
+  // only accept elements matching this CSS selector
+  accept: '#yes-drop',
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add('drop-active')
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget
+    var dropzoneElement = event.target
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target')
+    draggableElement.classList.add('can-drop')
+    draggableElement.textContent = 'Dragged in'
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target')
+    event.relatedTarget.classList.remove('can-drop')
+    event.relatedTarget.textContent = 'Dragged out'
+  },
+  ondrop: function (event) {
+    event.relatedTarget.textContent = 'Dropped'
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active')
+    event.target.classList.remove('drop-target')
+  }
+})
+
+interact('.drag-drop')
+  .draggable({
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    autoScroll: true,
+    // dragMoveListener from the dragging demo above
+    listeners: { move: dragMoveListener }
+  })
+
+// temp 
+
 </script>
 
 <h1> niveau {levelsRecipes[level].name} </h1>
+
+<!-- temp -->
+
+<div id="no-drop" class="drag-drop"> #no-drop </div>
+
+<div id="yes-drop" class="drag-drop"> #yes-drop </div>
+
+<div id="outer-dropzone" class="dropzone">
+  #outer-dropzone
+  <div id="inner-dropzone" class="dropzone">#inner-dropzone</div>
+ </div>
+
+<!-- temp -->
 
 <div>
 	<b>MixingBowl</b>
@@ -93,10 +177,63 @@
 		background: orange;
 		color: white;
 	}
-  ul {
+  	ul {
 		border: solid lightgray 1px;
 		display: flex; /* required for drag & drop to work when .item display is inline */
 		height: 40px; /* needed when empty */
 		padding: 10px;
 	}
+
+	/* temp */
+
+	#outer-dropzone {
+	height: 200px;
+	}
+
+	#inner-dropzone {
+	height: 140px;
+	}
+
+	.dropzone {
+	background-color: #cfb41b;
+	border: dashed 4px transparent;
+	border-radius: 4px;
+	margin: 10px auto 30px;
+	padding: 10px;
+	width: 80%;
+	transition: background-color 0.3s;
+	}
+
+	:global(.drop-active) {
+	border-color: rgb(240, 50, 50);
+	}
+
+	:global(.drop-target) {
+	background-color: rgb(157, 196, 223);
+	border-color: #fff;
+	border-style: solid;
+	}
+
+	.drag-drop {
+	display: inline-block;
+	min-width: 40px;
+	padding: 2em 0.5em;
+	margin: 1rem 0 0 1rem;
+
+	color: #fff;
+	background-color: #29e;
+	border: solid 2px #fff;
+
+	touch-action: none;
+	transform: translate(0px, 0px);
+
+	transition: background-color 0.3s;
+	}
+
+	:global(.drag-drop.can-drop) {
+	color: #000;
+	background-color: #4e4;
+	}
+
+/* temp */
 </style>
