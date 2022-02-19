8ae9fbf4eb12f8d7e97238ef42d3ad60c4c4ab0f
Amalia <amalia.pluchard@gmail.com>
2022-02-19 18:32:45 +0100

    Squashed commit of the following:
    commit 24aa2bf28415a760fc178befe9538fc9c7f04465
    Merge: 3b2cb4d c2a4834
    Author: Amalia <amalia.pluchard@gmail.com>
    Date:   Sat Feb 19 18:32:23 2022 +0100
    
        Merge branch 'main' into dragNdrop
    
    commit 3b2cb4de9992a95787a9e47563982ad4bf1c7631
    Author: Lilou Alidor <lilou.alidor@gmail.com>
    Date:   Sat Feb 19 18:05:01 2022 +0100
    
        DragNDrop completed
    
    commit 2b58426a9c4415a1bd364c68ba4557368ff19119
    Author: dsmtE <desmet.enguerrand@gmail.com>
    Date:   Sat Feb 19 15:13:17 2022 +0100
    
        fix global style not work
    
    commit 8e978b3cc3cf87e3ff7c3ce4e16537300cdc0d9f
    Author: Lilou Alidor <lilou.alidor@gmail.com>
    Date:   Sat Feb 19 14:47:07 2022 +0100
    
        WIP

--- src/Merge.svelte
+++ src/Merge.svelte
@@ -1,92 +1,141 @@
 <script>
   import { ComputeMixing } from "./core/recipesUtils";
   import levelsRecipes from "./data/levels_data";
-  import { flip } from "svelte/animate";
+  import interact from "interactjs";
   import { createEventDispatcher } from "svelte";
+  import { get } from "svelte/store";
+  import { level } from "./stores.js";
 
   const dispatch = createEventDispatcher();
 
-  let level = "Ballon perché";
+  let currentLevel = get(level);
 
-  // copy startingItems in current inventory
-  let inventory = [...levelsRecipes[level].startingItems];
+  let inventory = [...currentLevel.startingItems];
 
-  export let mixingBowl = [];
+  let ingredients = [];
+  let mixingBowl = [];
 
-  function handleDragStartFromInventory(event, itemIndex) {
-    const data = {
-      itemIndex: itemIndex,
-    };
-    event.dataTransfer.setData("text/plain", JSON.stringify(data));
-  }
-
-  function handleDragEndFromInventory(event, itemIndex) {
-    event.preventDefault();
-  }
-
-  function clearMixingBowl() {
+  function resetMove() {
+    ingredients.forEach((element) => {
+      element.style.transform = "translate(0px, 0px)";
+      element.setAttribute("data-x", "0");
+      element.setAttribute("data-y", "0");
+      if (element.classList.contains("can-drop")) {
+        // remove the drop feedback style
+        element.classList.remove("can-drop");
+      }
+    });
     mixingBowl = [];
   }
 
-  function handleDragDropToMixingBowl(event) {
-    event.preventDefault();
-
-    const data = JSON.parse(event.dataTransfer.getData("text/plain"));
-
-    mixingBowl = new Array(
-      ...new Set([...mixingBowl, inventory[data.itemIndex]])
-    );
-
-    const mixingResult = ComputeMixing(
-      mixingBowl,
-      levelsRecipes[level].recipes
-    );
+  function newElem() {
+    //new elem ?
+    const mixingResult = ComputeMixing(mixingBowl, currentLevel.recipes);
     if (mixingResult !== undefined) {
       mixingBowl = [];
       inventory = new Array(...new Set([...inventory, ...mixingResult]));
+      resetMove();
       dispatch("newItem", mixingResult);
     }
   }
+
+  //DRAG N DROP
+
+  function dragMoveListener(event) {
+    var target = event.target;
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+    var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+    // translate the element
+    target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+    // update the posiion attributes
+    target.setAttribute("data-x", x);
+    target.setAttribute("data-y", y);
+  }
+
+  // enable draggables to be dropped into this
+  interact(".dropzone").dropzone({
+    // only accept elements matching this CSS selector
+    accept: "#item",
+    // Require a 75% element overlap for a drop to be possible
+    overlap: 0.75,
+
+    // listen for drop related events:
+
+    ondropactivate: function (event) {
+      // add active dropzone feedback
+      event.target.classList.add("drop-active");
+    },
+    ondragenter: function (event) {
+      var draggableElement = event.relatedTarget;
+      var dropzoneElement = event.target;
+
+      // feedback the possibility of a drop
+      dropzoneElement.classList.add("drop-target");
+      draggableElement.classList.add("can-drop");
+    },
+    ondragleave: function (event) {
+      // remove the drop feedback style
+      event.target.classList.remove("drop-target");
+      event.relatedTarget.classList.remove("can-drop");
+      //delete object
+      let item = event.relatedTarget.textContent.slice(0, -1);
+      mixingBowl.forEach((element, i) => {
+        if (element == item) {
+          mixingBowl.splice(i, 1);
+        }
+      });
+      newElem();
+    },
+    ondrop: function (event) {
+      //add object
+      let item = event.relatedTarget.textContent.slice(0, -1);
+      mixingBowl = new Array(...new Set([...mixingBowl, item]));
+      newElem();
+    },
+    ondropdeactivate: function (event) {
+      // remove active dropzone feedback
+      event.target.classList.remove("drop-active");
+      event.target.classList.remove("drop-target");
+    },
+  });
+
+  interact(".drag-drop").draggable({
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: "parent",
+        endOnly: true,
+      }),
+    ],
+    autoScroll: true,
+    // dragMoveListener from the dragging demo above
+    listeners: { move: dragMoveListener },
+  });
 </script>
 
-<h1>niveau {levelsRecipes[level].name}</h1>
+<h1>niveau {currentLevel.name}</h1>
 
 <div>
   <b>MixingBowl</b>
-  <button on:click={(e) => clearMixingBowl()}> clear </button>
-  <ul
-    id="mixingBowl"
-    on:drop={(e) => handleDragDropToMixingBowl(e)}
-    ondragover="return false"
-  >
-    {#each mixingBowl as item, itemIndex (item)}
-      <div class="item" animate:flip>
-        <li>
-          {item}
-        </li>
-      </div>
-    {/each}
-  </ul>
-</div>
+  <button on:click={(e) => resetMove()}> Ranger la table </button>
+  <div id="MixingBowl" class="dropzone" />
 
-<div>
   <b>Inventory</b>
-  <ul>
-    {#each inventory as item, itemIndex (item)}
-      <div class="item" animate:flip>
-        <li
-          draggable={true}
-          on:dragstart={(e) => handleDragStartFromInventory(e, itemIndex)}
-          on:dragend={(e) => handleDragEndFromInventory(e, itemIndex)}
-        >
-          {item}
-        </li>
-      </div>
-    {/each}
-  </ul>
+  {#each inventory as item, itemIndex (item)}
+    <div id="item" class="drag-drop" bind:this={ingredients[itemIndex]}>
+      {item}
+    </div>
+  {/each}
 </div>
 
 <style>
+  .item {
+    display: inline; /* required for flip to work */
+  }
+
   .item {
     display: inline; /* required for flip to work */
   }
@@ -107,5 +156,51 @@
     display: flex; /* required for drag & drop to work when .item display is inline */
     height: 40px; /* needed when empty */
     padding: 10px;
+  }
+
+  #MixingBowl {
+    height: 140px;
+  }
+
+  .dropzone {
+    background-color: #cfb41b;
+    border: dashed 4px transparent;
+    border-radius: 4px;
+    margin: 10px auto 30px;
+    padding: 10px;
+    width: 80%;
+    transition: background-color 0.3s;
+  }
+
+  div:global(.drop-active) {
+    border-color: rgb(240, 50, 50);
+  }
+
+  div:global(.drop-target) {
+    background-color: rgb(157, 196, 223);
+    border-color: #fff;
+    border-style: solid;
+  }
+
+  .drag-drop {
+    display: inline-block;
+    min-width: 40px;
+    max-height: 45px;
+    padding: 2em 0.5em;
+    margin: 1rem 0 0 1rem;
+
+    color: #fff;
+    background-color: #29e;
+    border: solid 2px #fff;
+
+    touch-action: none;
+    transform: translate(0px, 0px);
+
+    transition: background-color 0.3s;
+  }
+
+  div:global(.drag-drop.can-drop) {
+    color: #000;
+    background-color: #4e4;
   }
 </style>
