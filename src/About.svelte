12664c0369da4d98651f08176150fac310c9c032
Amalia <amalia.pluchard@gmail.com>
2022-02-20 19:02:25 +0100

    update: about

--- src/About.svelte
+++ src/About.svelte
@@ -2,17 +2,20 @@
   import { Article } from "spaper";
   import shuffleArray from "./core/shuffleArray";
   let about = [
-  { name : 'Jules', website : 'https://julesfouchy.github.io/home/' },
-  { name : 'Anaïs', website : 'https://www.instagram.com/talia_kah' },
-  { name : 'Clémence', website : 'https://www.instagram.com/poulpillow/' },
-  { name : 'Lilou', website : 'https://www.instagram.com/miss_bidule/' },
-  { name : 'Amalia', website : '#' },
-  { name : 'Enguerrand', website : 'https://www.instagram.com/enguerrand_desmet/' },
-  { name : 'Emma', website : 'https://emmapernin.fr/' },
-  { name : 'Aurore', website : 'https://www.instagram.com/_just_kiel_/' }
-    ];
-    console.log(about)
-  shuffleArray(about)
+    { name: "Jules", website: "https://julesfouchy.github.io/home/" },
+    { name: "Anaïs", website: "https://www.instagram.com/talia_kah" },
+    { name: "Clémence", website: "https://www.instagram.com/poulpillow/" },
+    { name: "Lilou", website: "https://www.instagram.com/miss_bidule/" },
+    { name: "Amalia", website: "https://github.com/mmmaxou" },
+    {
+      name: "Enguerrand",
+      website: "https://www.instagram.com/enguerrand_desmet/",
+    },
+    { name: "Emma", website: "https://emmapernin.fr/" },
+    { name: "Aurore", website: "https://www.instagram.com/_just_kiel_/" },
+  ];
+  console.log(about);
+  shuffleArray(about);
 </script>
 
 <div class="main">
