1c7521b104bd1a19513c4834a27b90c5c10cbfcc
Amalia <amalia.pluchard@gmail.com>
2022-02-20 17:46:28 +0100

    update: about

--- src/About.svelte
+++ src/About.svelte
@@ -1,34 +1,34 @@
 <script>
-  import { Article } from "spaper"
-  import shuffleArray from "./core/shuffleArray"
+  import { Article } from "spaper";
+  import shuffleArray from "./core/shuffleArray";
   let about = [
-  { name : 'Jules', website : 'https://julesfouchy.github.io/home/' },
-  { name : 'Anaïs', website : '#' },
-  { name : 'Clémence', website : 'https://www.instagram.com/poulpillow/' },
-  { name : 'Lilou', website : 'https://www.instagram.com/miss_bidule/' },
-  { name : 'Amalia', website : '#' },
-  { name : 'Enguerrand', website : '#' },
-  { name : 'Emma', website : '#' },
-  { name : 'Aurore', website : '#' }
-    ];
-    console.log(about)
-  shuffleArray(about)
+    { name: "Jules", website: "https://julesfouchy.github.io/home/" },
+    { name: "Anaïs", website: "#" },
+    { name: "Clémence", website: "https://www.instagram.com/poulpillow/" },
+    { name: "Lilou", website: "https://www.instagram.com/miss_bidule/" },
+    { name: "Amalia", website: "https://github.com/mmmaxou" },
+    { name: "Enguerrand", website: "#" },
+    { name: "Emma", website: "#" },
+    { name: "Aurore", website: "#" },
+  ];
+  console.log(about);
+  shuffleArray(about);
 </script>
 
 <div class="main">
-    <Article title={"Les alchimistes :"} border>
-        <ul>
-            {#each about as { name, website}}
-                <li>
-                    {#if website != '#'}
-                        <a target="_blank" href="{website}">{name}</a>
-                    {:else}
-                        {name}
-                    {/if}
-                </li>
-            {/each}
-        </ul>
-    </Article>
+  <Article title={"Les alchimistes :"} border>
+    <ul>
+      {#each about as { name, website }}
+        <li>
+          {#if website != "#"}
+            <a target="_blank" href={website}>{name}</a>
+          {:else}
+            {name}
+          {/if}
+        </li>
+      {/each}
+    </ul>
+  </Article>
 
-    <br/>
+  <br />
 </div>
