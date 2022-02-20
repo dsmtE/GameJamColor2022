29effde77044c3922ed0ce38b3ee5b72ab64e875
Jules Fouchy <jules.fouchy@ntymail.com>
2022-02-20 04:24:47 +0100

    About : RAJOUTEZ VOS LIENS

--- src/About.svelte
+++ src/About.svelte
@@ -1,15 +1,18 @@
 <script>
-  import { gameName, flowScene, introText } from "./stores";
-  import { Article, Button } from "spaper"
+  import { Article } from "spaper"
 </script>
 
 <div class="main">
     <Article title={"Les alchimistes :"} border>
         <ul>
-            <li>Jules</li>
+            <li><a href="https://julesfouchy.github.io/home/">Jules</a></li>
             <li>Anaïs</li>
-            <li>Clémence</li>
+            <li><a href="https://www.instagram.com/poulpillow/">Clémence</a></li>
             <li>Lilou</li>
+            <li>Amalia</li>
+            <li>Enguerrand</li>
+            <li>Emma</li>
+            <li>Aurore</li>
         </ul>
     </Article>
 
