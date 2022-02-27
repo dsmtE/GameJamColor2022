33d020c9f57c62fc6e408c6a9188a70723b423b6
dsmtE <enguerrand.desmet@gmail.com>
2022-02-27 19:29:24 +0100

    add copy to doc post processor

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
 import css from 'rollup-plugin-css-only'
+import copy from 'rollup-plugin-copy'
 
 const production = !process.env.ROLLUP_WATCH
 
@@ -72,7 +73,11 @@ export default {
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
-    production && terser()
+    production && terser(),
+
+    production && copy({
+      targets: [{ src: 'public/*', dest: 'docs/' }],
+    }),
   ],
   watch: {
     clearScreen: false
